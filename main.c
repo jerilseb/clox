4446d87ed98b02a952da47da59bef9ae7b2fb55a
Jeril Sebastian <seb988@gmail.com>
2020-04-02 17:13:59 +0530

    bytecode vm implemented

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include "common.h"       
 #include "chunk.h"              
 #include "debug.h"
+#include "vm.h"   
 
 int main(int argc, const char* argv[]) {
+  initVM();
+
   Chunk chunk;                          
   initChunk(&chunk);                    
 
   int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT);        
-  writeChunk(&chunk, constant);
+  writeChunk(&chunk, OP_CONSTANT, 122);        
+  writeChunk(&chunk, constant, 122);
 
   constant = addConstant(&chunk, 435);
-  writeChunk(&chunk, OP_CONSTANT);        
-  writeChunk(&chunk, constant);         
+  writeChunk(&chunk, OP_CONSTANT, 123);        
+  writeChunk(&chunk, constant, 123);         
 
-  writeChunk(&chunk, OP_RETURN); 
-  disassembleChunk(&chunk, "Test chunk");
+  writeChunk(&chunk, OP_RETURN, 123); 
 
+  interpret(&chunk);
+
+  freeVM();
   freeChunk(&chunk); 
   return 0;                             
 }
