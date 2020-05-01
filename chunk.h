4124aa587eca27116c523dea5e6d1cdc9befdb2e
Jeril Sebastian <seb988@gmail.com>
2020-05-01 21:35:05 +0530

    handle comparison operators and bools

--- chunk.h
+++ chunk.h
@@ -11,8 +11,15 @@ typedef enum {
     OP_SUBTRACT,
     OP_MULTIPLY,
     OP_DIVIDE, 
+    OP_NOT,
     OP_NEGATE,
-    OP_CONSTANT
+    OP_CONSTANT,
+    OP_NIL,     
+    OP_TRUE,    
+    OP_FALSE,
+    OP_EQUAL,  
+    OP_GREATER,
+    OP_LESS,
 } OpCode;
 
 typedef struct {
